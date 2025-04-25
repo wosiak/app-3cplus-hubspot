927549a2dd9fbe84748e98e72849a684e7c8aed7
wosiak <eduu.wosiak@gmail.com>
2025-04-25 11:28:39 -0300

    Adiciona headers CSP para liberar iframe via HubSpot

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.vercel.app https://*.hubspot.com https://app.hubspot.com",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
